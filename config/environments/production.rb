8301dc47e44cb695033210c0537619df0167a76f
Yevhenii Kononenko <evgenijkononenko0@gmail.com>
2021-01-28 21:30:14 +0200

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,7 @@ Rails.application.configure do
     port:                 587,
     domain:               'gmail.com',
     user_name:            'xstore986@gmail.com',
-    password:             "Dembele11",
+    password:             Rails.application.credentials.gmail_password,
     authentication:       'plain',
     enable_starttls_auto: true
   }
