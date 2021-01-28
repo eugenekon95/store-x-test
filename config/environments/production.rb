b6be70ba08addee9195e486e05169b5f417f8679
Yevhenii Kononenko <evgenijkononenko0@gmail.com>
2021-01-28 17:50:49 +0200

    rollback

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,7 @@ Rails.application.configure do
     port:                 587,
     domain:               'gmail.com',
     user_name:            'xstore986@gmail.com',
-    password:             'amY4XNF&8Ack',
+    password:             Rails.application.credentials.gmail_password,
     authentication:       'plain',
     enable_starttls_auto: true
   }
