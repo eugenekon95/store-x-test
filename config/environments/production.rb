f4e7a91f7b64117e79bd160d4ee4625549e35489
Yevhenii Kononenko <evgenijkononenko0@gmail.com>
2021-01-28 17:48:08 +0200

    mail update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,9 +20,9 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'example.com',
+    domain:               'gmail.com',
     user_name:            'xstore986@gmail.com',
-    password:             Rails.application.credentials.gmail_password,
+    password:             'amY4XNF&8Ack',
     authentication:       'plain',
     enable_starttls_auto: true
   }
